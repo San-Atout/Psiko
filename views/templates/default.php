6a132993ef63730921b79981101f3d6881980ade
Augustin Rousset Rouviere <augustin.rousset-rouviere@isep.fr>
2020-03-20 14:16:23 +0100

    Systeme de changement de langue
    Fin du système de changement de langue avec pour l'instant 4 langues (Polonais, Arabe, Anglais et Français) Les pages ne sont bien sûr pas traduites

--- views/templates/default.php
+++ views/templates/default.php
@@ -5,5 +5,15 @@
   created by : Augustin ROUSSET-ROUVIERE 
   fichier php contenant le gabari par défault du site web
 */
+$lienAr = isset($autrePages["ar"])? $autrePages["ar"] : $routeur->getUrl("404 ar");
+$lienFr = isset($autrePages["fr"])? $autrePages["fr"] : $routeur->getUrl("404 fr");
+$lienEn = isset($autrePages["en"])? $autrePages["en"] : $routeur->getUrl("404 en");
+$lienPl = isset($autrePages["pl"])? $autrePages["pl"] : $routeur->getUrl("404 pl");
+?>
 
-
+<ul>
+    <li><a href="<?=$lienFr?>">vers le site en fr</a></li>
+    <li><a href="<?=$lienEn?>">vers le site en en</a></li>
+    <li><a href="<?=$lienAr?>">vers le site en ar</a></li>
+    <li><a href="<?=$lienPl?>">vers le site en pl</a></li>
+</ul>
