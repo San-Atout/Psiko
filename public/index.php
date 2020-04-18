6a4d053b8d2d2562fb1e61c674e97c73046a98dd
Gwald08 <58332447+Gwald08@users.noreply.github.com>
2020-04-18 20:36:07 +0200

    zf

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 <?php 
 /**
 création de l'index PHP
+=======
+<?php 
+/**
+création de l'index PHP
+>>>>>>> Stashed changes
 **/
--- src/database/UserTable.php
+++ src/database/UserTable.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 
@@ -65,4 +66,73 @@ class userTable
     {
         return $this->database->query("SELECT * FROM user");
     }
+=======
+<?php
+
+
+namespace Psiko\database;
+
+
+use Psiko\Entity\userEntity;
+
+class userTable
+{
+
+    /**
+     * @var MysqlDatabase
+     */
+    private MysqlDatabase $database;
+
+    public function __construct()
+    {
+       $this->database = new MysqlDatabase("psiko");
+    }
+
+    public function getUserByMail (String $mail)
+    {
+        $sql = "SELECT * FROM psiko.user WHERE email=:email";
+        return $this->database->prepare($sql, array(":email" => $mail));
+    }
+
+
+    public function isUserInDatabase($prenom, $nom, $email)
+    {
+        $sql = "SELECT * FROM psiko.user WHERE (`nom` = :nom AND `prenom` = :prenom) OR (`email` = :email)";
+        return empty($this->database->prepare($sql, array(":prenom" => $prenom, ":nom" => $nom, ":email" => $email)));
+    }
+
+    public function insertNewUser(userEntity $user)
+    {
+        $sql = "INSERT INTO psiko.user (`prenom`, `nom`, `email`, `adresse`, `telephone`, `sexe`, `password`, `dateInscription`, `birthday`, `ecoleId`, `rang`, `valider`, `photoPicture`) 
+                       VALUES (:prenom, :nom, :email, :adresse, :telephone, :sexe, :password, :dateInscription, :birthday, :ecoleId, :rang, :valider, :photoPicture)";
+        $values = array(
+            ":prenom"           => $user->getPrenom(),
+            ":nom"              => $user->getNom(),
+            ":email"            => $user->getEmail(),
+            ":adresse"          => $user->getAdresse(),
+            "telephone"         => $user->getTelephone(),
+            ":sexe"             => $user->getSexe(),
+            ":password"         => $user->getPassword(),
+            ":dateInscription"  => $user->getDateInscription()->format('Y-m-d'),
+            ":birthday"         => $user->getBirthday()->format('Y-m-d'),
+            ":ecoleId"          => $user->getEcoleId(),
+            ":rang"             => $user->getRang() ,
+            ":valider"          => $user->isValider(),
+            ":photoPicture"     => $user->getPhotoPicture()
+        );
+        $this->database->prepare($sql,$values);
+
+    }
+
+    public function getUserById($id)
+    {
+        $prepare = "SELECT * FROM psiko.user WHERE `id` = :id";
+        return $this->database->prepare($prepare,array(":id"=>$id))[0];
+    }
+
+    public function getAllUser()
+    {
+        return $this->database->query("SELECT * FROM user");
+    }
+>>>>>>> Stashed changes
 }
