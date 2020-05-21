a27cebcb78db28ffe238c1d453271fc72ba333e5
Augustin Rousset Rouviere <augustin.rousset-rouviere@isep.fr>
2020-05-21 17:07:54 +0200

    Modif sécu écoles

--- views/fr/admin/ecoles/ecolesBasique.php
+++ views/fr/admin/ecoles/ecolesBasique.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION["auth"])) {
     header("Location: /fr/connexion/");
     exit();
 }
-if ($_SESSION["auth"]->getRang() != "administrateur" && $_SESSION["auth"]->getRang() != "gestionnaire") {
+if ($_SESSION["auth"]->getRang() != "administrateur") {
     header("Location: /fr/401");
     exit();
 }
